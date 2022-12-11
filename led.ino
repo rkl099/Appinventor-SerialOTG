9a6a1c32bf6c35e13872e614faeda944dd88cf37
rkl099 <rkardell89@gmail.com>
2022-12-11 21:49:06 +0100

    Add files via upload

--- led.ino
+++ led.ino
@@ -1,17 +1,26 @@
-
-#define led 13 //LED pin on UNO, HopeDuino
-//#define led 9  //Moteino
-
-void setup() {
-   pinMode(led,OUTPUT);
+/*
+ * Test program, echo serial in 
+ * if "1", LED on. if "0" LED off
+ */
+#define led LED_BUILTIN 
+// #define led 17          //ProMicro Only blink
+ 
+ void setup()  
+ {  
+  Serial.begin(9600);  
   
-
-}
-
-
-void loop() {
-     digitalWrite(led,LOW);
-     delay(1000);
-     digitalWrite(led,HIGH);
-     delay(1000);
-}
+  pinMode(led, OUTPUT);
+ } 
+  
+ void loop()  
+ {  
+  char c;
+  if(Serial.available())  
+  {  
+   c = Serial.read();  
+     Serial.print(c);  
+     if (c==0x31) digitalWrite(led, HIGH);
+     if (c==0x30) digitalWrite(led, LOW);
+     
+  }  
+ }  
